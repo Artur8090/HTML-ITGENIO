38462a292613588c294d38f5e42aa68aaf075931
Artur8090 <artur.cheuchiev@gmail.com>
2023-11-03 17:06:27 +0100

    JavaScript start

---
+++ JS/First steps/P2/script.js
@@ -0,0 +1,23 @@
+let count = 700
+alert(count)
+
+let age = 12;
+age = 13;
+alert(age)
+
+let language = "JS";
+let explain = " язык программирования";
+alert(language + explain)
+
+let num = 250;
+alert(3*num)
+
+let a = 400;
+let b = a + 3;
+let c = a + b;
+alert(c)
+
+let r = 5;
+const PI = 3.141592;
+alert(PI * (r ** 2) )
+alert(2 * PI * r)
---
+++ JS/First steps/P3/script.js
@@ -0,0 +1,25 @@
+let name = prompt("Сколько вам лет?");
+let conf = confirm(`Вам точно ${name} лет?`)
+alert(`Вам ${name} лет`)
+
+let a = 5
+a = a * 2 
+console.log(a)
+a = a * 2 
+console.log(a)
+a = a * 2 
+console.log(a)
+a = a * 2 
+console.log(a)
+a = a * 2 
+console.log(a)
+
+let x = prompt("Впишите значение х")
+let y = -5 * x + 2
+console.log(`При x = ${x}, значение y = ${y}`)
+
+let count = 7;
+let time = 30;
+let name2 = 'Nikita';
+
+console.log(`Игрок: ${name} на ${count} секунде игры набрал ${time} очков.`)
