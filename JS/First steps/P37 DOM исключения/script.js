481a9bdeff3158c4c55510c4573871756045942a
Artur8090 <artur.cheuchiev@gmail.com>
2024-03-05 17:16:28 +0100

    DOM continue

--- JS/First steps/P37 DOM исключения/script.js
+++ JS/First steps/P37 DOM исключения/script.js
@@ -20,6 +20,70 @@ button2.addEventListener('click',function(){
     console.log(arr)
 
 })
-*/
+
 let img = document.querySelector('#image');
 console.log(img.src, img.width, img.height)
+
+
+
+
+function func() {
+    this.value = Number(this.value) + 1;
+}
+
+let elems = document.querySelectorAll('input');
+console.log(elems)
+for (let elem of elems) {
+    elem.addEventListener('blur', func);
+}
+
+function func() {
+    this.innerHTML = Number(this.innerHTML) ** 2;
+    console.log(this.value)
+}
+
+let elems = document.querySelectorAll('p');
+console.log(elems)
+for (let elem of elems) {
+    elem.addEventListener('click', func);
+}
+
+let divs = document.querySelectorAll('div');
+
+for (let div of divs) {
+	div.addEventListener('click', function(){
+        this.textContent++;
+    });
+}
+
+
+let link = document.querySelector('#link');
+let button = document.querySelector('#button');
+button.addEventListener('click',func)
+
+function func(){
+    link.innerHTML += link.href
+    button.removeEventListener('click',func)
+}
+
+let buttonNum = 0;
+let button = document.querySelector('#button');
+button.addEventListener('click',func)
+
+function func(){
+if(buttonNum <= 8){
+    button.value= Number(button.value) + 1;
+    buttonNum++;
+} else if(buttonNum > 10){
+    button.removeEventListener('click',func)
+}
+}
+*/
+function func(){
+    this.textContent= this.textContent+'!';
+    this.removeEventListener('click',func)
+}
+let elems = document.querySelectorAll('p');
+for(elem of elems){
+    elem.addEventListener('click',func)
+}
