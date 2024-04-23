77a21cd75e7ae677804e75c2dd98e9ae10de89e9
Artur8090 <artur.cheuchiev@gmail.com>
2024-04-23 17:27:21 +0200

    Проект сайдбар

--- JS/First steps/Проект Memory Game/script.js
+++ JS/First steps/Проект Memory Game/script.js
@@ -1,19 +1,25 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 
 function flipCard() {
-    this.classList.toggle('flip')
+    if(lockBoard) return;
+    if(this === firstCard) return;
+    this.classList.toggle('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+    } else {
+        secondCard = this;
+        checkForMatch();
     }
-
-    secondCard = this;
-    hasFlippedCard = false;
-    checkForMatch();
+}
+function resetBoard(){
+    [hasFlippedCard,lockBoard] = [false,false]
+    [firstCard,secondCard] = [null,null]
 }
 function checkForMatch() {
     if (firstCard.dataset.itgenik === secondCard.dataset.itgenik) {
@@ -24,12 +30,21 @@ function checkForMatch() {
 }
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 function unflipCards() {
     setTimeout(() => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
+        resetBoard();
     }, 1500);
 }
+(function shufle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 cards.forEach(card => card.addEventListener('click', flipCard))
