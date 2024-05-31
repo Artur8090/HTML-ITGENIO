9ae5c1544bd5582e06e621137bb2d5d63464183d
Artur8090 <artur.cheuchiev@gmail.com>
2024-05-31 17:20:41 +0200

    Canvas project

--- JS/First steps/P50 Canvas Practice 2/script.js
+++ JS/First steps/P50 Canvas Practice 2/script.js
@@ -30,24 +30,47 @@ ctx.translate(125,0)
 ctx.fillStyle = 'blue';
 ctx.fillRect(50,50,50,50)
 */
-
+/*
 ctx.fillStyle = 'blue';
-ctx.fillRect(200,50,75,25)
-ctx.fillStyle = 'red'
-ctx.fillRect(275,50,75,25)
+ctx.fillRect(200, 50, 75, 25);
+ctx.fillStyle = 'red';
+ctx.fillRect(275, 50, 75, 25);
 
-ctx.rotate(getRadians(50))
-ctx.translate(50,0);
-ctx.fillStyle = 'blue';
-ctx.fillRect(200,50,75,25)
-ctx.fillStyle = 'red'
-ctx.fillRect(275,50,75,25)
-
-
-
-
-ctx.stroke()
-
-function getRadians(degrees){
-    return Math.PI/180*degrees
+// Function to convert degrees to radians
+function getRadians(degrees) {
+    return Math.PI / 180 * degrees;
 }
+
+// Rectangle rotated 90 degrees
+ctx.save();
+ctx.translate(400, 75); // Adjusted to position the rotation center correctly
+ctx.rotate(getRadians(90));
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, 75, 25);
+ctx.fillStyle = 'red';
+ctx.fillRect(75, 0, 75, 25);
+ctx.restore();
+
+// Rectangle rotated 90 degrees and then 180 degrees (270 degrees in total)
+ctx.save();
+ctx.translate(600, 75); // Position the third rectangle to the right of the second one
+ctx.rotate(getRadians(270)); // 90 degrees + 180 degrees = 270 degrees
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, -75, 75, 25); // Corrected to ensure correct positioning
+ctx.fillStyle = 'red';
+ctx.fillRect(75, -75, 75, 25);
+ctx.restore();
+*/
+ctx.fillStyle = 'blue';
+ctx.fillRect(25,25,100,50)
+ctx.translate(35,17)
+ctx.stroke();
+ctx.fillStyle = 'rgba(0,0,255,0.50)'
+ctx.scale(1.25,1.25)
+ctx.fillRect(25,25,100,50)
+ctx.translate(35,17)
+ctx.stroke();
+ctx.fillStyle = 'rgba(0,0,255,0.25)'
+ctx.scale(1.5,1.5)
+ctx.fillRect(25,25,100,50)
+ctx.stroke();
---
+++ JS/First steps/Проект простая программа рисование/script.js
@@ -0,0 +1,61 @@
+let canvas;
+let ctx;
+
+window.onload = function(){
+    canvas = document.getElementById('drawingCanvas');
+    ctx = canvas.getContext('2d')
+
+    canvas.onmousedown = startDrawing;
+    canvas.onmouseup = stopDrawing;
+    canvas.onmouseout = stopDrawing;
+    canvas.onmousemove = draw;
+
+}
+
+let previousColorElement;
+function changeColor(color, imgElement){
+    ctx.strokeStyle = color;
+    imgElement.className = 'Selected';
+    if(previousColorElement != null){
+        previousColorElement.className = '';
+    }
+    previousColorElement = imgElement
+}
+let previousThicknessElement;
+function changeThickness(thickness, imgElement){
+    ctx.lineWidth = thickness;
+    imgElement.className = 'Selected';
+    if(previousThicknessElement != null){
+        previousThicknessElement.className = '';
+    }
+    previousThicknessElement = imgElement
+}
+
+function startDrawing(e){
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop)
+}
+
+function draw(e){
+    if(isDrawing == true){
+        let x = e.pageX - canvas.offsetLeft;
+        let y = e.pageY - canvas.offsetTop;
+
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+function stopDrawing(){
+    isDrawing = false;
+}
+function clearCanvas(){
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+}
+function saveCanvas(){
+    let imageCopy = document.getElementById('savedImageCopy');
+
+    imageCopy.src= canvas.toDataUrl();
+    let imageContainer = document.getElementById('savedCopyContainer')
+    imageContainer.style.display = 'block'
+}
