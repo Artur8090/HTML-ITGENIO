463e00164c3dfd8dc1bf67af363b22a10c554db3
Artur8090 <artur.cheuchiev@gmail.com>
2024-07-02 17:12:50 +0200

    02.07.24

--- JS/First steps/P53 Jquery/script.js
+++ JS/First steps/P53 Jquery/script.js
@@ -1,13 +1,42 @@
-$(document).ready(function(){
-    let elems = $('h3')
-    for(let i  = 0; i <= elems.length - 1; i++){
-        elems[i].innerText = '!!!'
+$(document).ready(function() {
+    let find = $('li').not('.test');
+    let first = $('li').first();
+    let last = $('li').last();
+    let numFive = $('li').eq(5);
+    let nextToLast = $('li').eq($('li').length - 2);
+    let threeToTen = $('li').slice(2, 10); 
+    let filtered = $('li').filter('.www');
+    let prev = $('.test').prev();
+    let next = $('.test').next();
+    let allPrev = $('.test').prevAll();
+    let allNext = $('.test').nextAll();
+    let prevUntil = $('.www').prevUntil('.text');
+    let nextUntil = $('.text').nextUntil('.www');
+
+    function logElements(elements) {
+        elements.each(function(element) {
+            console.log($(element).text());
+        });
     }
-    elems.css('color','green')
-    elems.css('font-weight','bold')
 
-    let element = $('.zzz');
-    element.css('color','red')
-
-    $(':header').css('color', 'red')
-})
+    console.log('find:');
+    logElements(find);
+    console.log('first:', first.text());
+    console.log('last:', last.text());
+    console.log('numFive:', numFive.text());
+    console.log('nextToLast:', nextToLast.text());
+    console.log('threeToTen:');
+    logElements(threeToTen);
+    console.log('filtered:');
+    logElements(filtered);
+    console.log('prev:', prev.text());
+    console.log('next:', next.text());
+    console.log('allPrev:');
+    logElements(allPrev);
+    console.log('allNext:');
+    logElements(allNext);
+    console.log('prevUntil:');
+    logElements(prevUntil);
+    console.log('nextUntil:');
+    logElements(nextUntil);
+});
---
+++ JS/First steps/P54 jQuery attributes/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    let elem = $('h2');
+    /*
+    elem.wrap("<div class='test'></div>")
+    
+    elem.wrapInner('<i></i>')
+   
+    $('h2').wrapAll('<i></i>')
+    $('p').unwrap()
+    
+    $('h2').prepend('<p>!</p>').append('<p>?</p>')
+    
+    $('h2').prepend('!').append('?').css('color','red')
+   
+
+    elem.replaceWith(elem.text())
+    elem.replaceWith('p')
+     
+
+    elem.remove();
+    elem.empty();
+    */
+    console.log($('.www').attr('href'))
+    });
